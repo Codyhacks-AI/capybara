a73685f06cd06e7f2385b6c67e2de68fe2ba2121
Trevor Trinh <50549133+trevor-trinh@users.noreply.github.com>
2023-06-17 15:44:18 -0700

    setup front/backend

--- frontend/src/langchain/models.ts
+++ frontend/src/langchain/models.ts
@@ -5,7 +5,7 @@ import * as vscode from "vscode";
 dotenv.config();
 const openAIApiKey: string =
   "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";
-// export const defaultLLM = new OpenAI({
-//   openAIApiKey: `${env:OPENAI_API_KEY}`,
+export const defaultLLM = new OpenAI({
+  openAIApiKey: openAIApiKey,
   temperature: 0.9,
 });
