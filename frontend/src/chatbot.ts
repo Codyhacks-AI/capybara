6fb2ad4c559ae33ffdad94ea2181286d316bc6f3
Tommy Hang <bumberbee@Tommys-MacBook-Pro.local>
2023-06-18 01:38:31 -0700

    Add chatbot to history

---
+++ frontend/src/chatbot.ts
@@ -0,0 +1,45 @@
+import * as vscode from 'vscode';
+
+export const openChatBot = () => {
+  const panel = vscode.window.createWebviewPanel(
+    'customContainer',
+    'Custom Container',
+    { preserveFocus: true, viewColumn: vscode.ViewColumn.Two },
+    { enableScripts: true }
+  );
+  panel.webview.html = getWebviewContent();
+  panel.webview.onDidReceiveMessage(message => {
+    if (message.command === 'sendMessage') {
+      const text = message.text;
+      // Call your callback function with the received message text
+      console.log(text);
+    }
+  });
+};
+
+function getWebviewContent(): string {
+  return `
+    <html>
+      <body>
+        <h1>Chat Box</h1>
+        <input type="text" id="messageInput" placeholder="Type your message..." />
+        <button onclick="sendMessage()">Send</button>
+
+        <script>
+          const vscode = acquireVsCodeApi();
+
+          function sendMessage() {
+            const messageInput = document.getElementById('messageInput');
+            const message = messageInput.value;
+
+            // Call the callback function in the extension
+            vscode.postMessage({ command: 'sendMessage', text: message });
+
+            // Clear the input field
+            messageInput.value = '';
+          }
+        </script>
+      </body>
+    </html>
+  `;
+}
