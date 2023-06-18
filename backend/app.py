221562c01be4e15bce88c62343fe2488d8455e48
Charles Ming <charlesming@berkeley.edu>
2023-06-18 11:46:07 -0700

    forgot commit

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from recommend import get_suggestions
 
 app = Flask(__name__)
 
@@ -6,3 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/recommendations")
+def get_recommendations():
+    args = request.args
+    new_code = args.get('new_code')
+    return get_suggestions(new_code)
