509bab532a633985d39f051724ef4c9efd96df67
Tommy Hang <bumberbee@Tommys-MacBook-Pro.local>
2023-06-18 00:39:14 -0700

    HIGHLIGHT YELLO

--- frontend/src/callbacks.ts
+++ frontend/src/callbacks.ts
@@ -1,6 +1,8 @@
 import * as vscode from "vscode";
 import Flask from "./api/flask";
 import OpenAI from "./api/openai";
+import { OutputFunctionCall } from "./api/types";
+import { start } from "repl";
 
 export const onDocumentSave = async (document: vscode.TextDocument) => {
   vscode.window.showInformationMessage("Saving and checking in progress...");
@@ -9,6 +11,7 @@ export const onDocumentSave = async (document: vscode.TextDocument) => {
   const text = document.getText();
   await searchNameConventions(text);
   await searchDuplicates(text);
+  // highlightLine(6, 10, "ho", "hi");
   vscode.window.showInformationMessage("Saved and Logged Imporvements!");
 };
 
@@ -72,6 +75,8 @@ export const searchNameConventions = async (text: string) => {
         },
       },
     });
+    handleLines(res);
+    // return res;
     // console.log(res);
 
     // const res = await OpenAI.callFunction({
@@ -174,8 +179,79 @@ export const searchDuplicates = async (text: string) => {
       },
     });
     // console.log(res);
+    handleLines(res);
+    return res;
 
   } catch (e) {
     console.log(e);
   }
+};
+
+export const handleLines = async (linesToHighlight: OutputFunctionCall | undefined) => {
+  if (linesToHighlight !== undefined) {
+
+    const lineObject = linesToHighlight.arguments.linesToHighlight;
+    for (const line of lineObject) {
+      const startLine = line["startLine"];
+      const endLine = line["endLine"];
+      const reason = line["reason"];
+      const suggestion = line["suggestion"];
+
+      vscode.window.showInformationMessage(`Line ${startLine} to ${endLine} can be improved because ${reason}. Suggestion: ${suggestion}`);
+
+      const startNumber = await findLineNumber(line["startLine"]);
+      const endNumber = await findLineNumber(line["endLine"]);
+
+      highlightLine(startNumber, endNumber, reason, suggestion);
+    }
+  }
+};
+
+const findLineNumber = async (targetLineText: string) => {
+  const activeEditor = vscode.window.activeTextEditor;
+  if (activeEditor) {
+    const document = activeEditor.document;
+    for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+      const line = document.lineAt(lineNumber);
+      if (line.text === targetLineText) {
+        // Line found, return the line number (1-based indexing)
+        return lineNumber;
+      }
+    }
+  }
+  // Line not found
+  return -1;
+};
+
+const highlightLine = async (startNumber: number, endNumber: number, reason: string, suggestion: string) => {
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No active editor
+  }
+
+  let startPosition = new vscode.Position(startNumber, 0);
+  let endPosition = new vscode.Position(startNumber, 1000000);
+  if (startNumber >= 0 && startNumber < editor.document.lineCount) {
+    const startText = editor.document.lineAt(startNumber).text;
+    const startLength = startText.length;
+    endPosition = new vscode.Position(startNumber, startLength);
+  }
+
+  const highlightDecorationType = vscode.window.createTextEditorDecorationType({
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    overviewRulerColor: 'blue',
+    overviewRulerLane: vscode.OverviewRulerLane.Right,
+    light: {
+      // this color will be used in light color themes
+      borderColor: 'yellow',// Add this line to set the background color
+    },
+    dark: {
+      // this color will be used in dark color themes
+      borderColor: 'yellow', // Add this line to set the background color
+    }
+  });
+
+  const decoration = { range: new vscode.Range(startPosition, endPosition), hoverMessage: `You code is bad because: ${reason}\n My suggestion is: ${suggestion}` };
+  editor.setDecorations(highlightDecorationType, [decoration]);
 };
